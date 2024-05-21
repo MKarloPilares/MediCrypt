5ee8c99e66214c4bc8248f07276161a7353cb282
MKProjects <karlo29pilares@gmail.com>
2024-05-21 11:50:17 +0800

    I don't know anymore

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Container, Image } from 'react-bootstrap';
+import { Container, Image, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {ThemeProvider} from 'react-bootstrap';
@@ -12,14 +12,21 @@ const HomePage = ({setPage, connectMetamaskWallet, ethereumAccount}) => {
     breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
     minBreakpoint="xxs"
   >
-      <Container style={{position: 'relative', top: '50px', height: '60vh'}}>
-            <Image src={logo} style={{width: '80%', position: 'relative', left: '6%', bottom: '150px'}}/>
-            <Button variant='success' style={ethereumAccount === null ? ({position: 'fixed', left: '42%', width: '15%', bottom: '190px'}) : ({visibility: 'hidden'})}
-              onClick={connectMetamaskWallet}>Connect Metamask</Button>
-            <Button variant='success' style={ethereumAccount !== null ? ({position: 'sticky', left: '40%', width: '15%', bottom: '190px'}) : ({visibility: 'hidden'})}
+      <Container style={{position: 'relative', top: '50px', width: '100vw'}}>
+        <Row>
+            <Image src={logo} style={{width: '85%', position: 'sticky', left: '18.5%', marginBottom: '5%'}}/>
+        </Row>
+        <Row>
+            {ethereumAccount === null ? (
+            <Button variant='success' style={ethereumAccount === null ? ({position: 'fixed', right: '43%', width: '15%', top: '90%'}) : ({visibility: 'hidden'})}
+              onClick={connectMetamaskWallet}>Connect Metamask</Button>): (
+            <>
+            <Button variant='success' style={ethereumAccount !== null ? ({position: 'fixed', right: '52%', width: '7.5%', top: '90%'}) : ({visibility: 'hidden'})}
               onClick={() => setPage(1)}>Create Record</Button>             
-            <Button variant='success' style={ethereumAccount !== null? ({position: 'sticky', left: '50%', width: '15%', bottom: '190px'}) : ({visibility: 'hidden'})}
-              onClick={() => setPage(5)}>Records List</Button>    
+            <Button variant='success' style={ethereumAccount !== null? ({position: 'fixed', right: '42%', width: '7.5%', top: '90%'}) : ({visibility: 'hidden'})}
+              onClick={() => setPage(5)}>Records List</Button>
+            </>)}
+        </Row> 
       </Container>
   </ThemeProvider>
   );
