225b3162e22dc1f15a97c7119c9e8bb5017cc4f0
MKarloPilares <karlo29pilares@gmail.com>
2024-09-24 15:53:38 +0800

    Mobile Metamask

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    inject({
+      Buffer: ['buffer', 'Buffer'], // Polyfill Buffer for browser
+      process: 'process/browser', // Inject the process polyfill for browser
+    }),
+  ],
+  define: {
+    global: 'window', // Polyfill for global object in browsers
+    'process.env': {}, // Define process.env to avoid undefined errors
+  },
+});
