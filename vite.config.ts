98d0ec7fec37021a8ad24040de6e2a023dd55dcd
MKarloPilares <karlo29pilares@gmail.com>
2024-09-24 16:02:27 +0800

    don't know anymore

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import inject from '@rollup/plugin-inject';
+import commonjs from '@rollup/plugin-commonjs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     inject({
-      Buffer: ['buffer', 'Buffer'], // Polyfill Buffer for browser
-      process: 'process/browser', // Inject the process polyfill for browser
+      process: 'process/browser', // Inject the process polyfill for the browser
+      Buffer: ['buffer', 'Buffer'], // Polyfill Buffer
     }),
+    commonjs(), // Add CommonJS plugin to handle CommonJS modules like invariant
   ],
   define: {
-    global: 'window', // Polyfill for global object in browsers
+    global: 'window', // Polyfill for global in browsers
     'process.env': {}, // Define process.env to avoid undefined errors
   },
 });
