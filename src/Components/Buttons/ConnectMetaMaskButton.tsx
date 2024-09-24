02cc3a999c32fde7b0a236ee33a4102d2d4ac7c8
MKarloPilares <karlo29pilares@gmail.com>
2024-09-24 16:54:12 +0800

    Back to start

--- src/Components/Buttons/ConnectMetaMaskButton.tsx
+++ src/Components/Buttons/ConnectMetaMaskButton.tsx
@@ -15,35 +15,23 @@ const ConnectMetaMaskButton: React.FC<ConnectMetaMaskButtonProps> = ({ setUserWa
   
   const connectMetamaskWallet = async () => {
     if (typeof (window as any).ethereum === 'undefined') {
+      // Redirect to MetaMask on mobile
       window.open('https://metamask.app.link/', '_blank');
       return;
     }
   
     try {
-      // Request permission for accounts access
-      await (window as any).ethereum.request({ method: "wallet_requestPermissions", params: [{ eth_accounts: {} }] });
-  
-      // Ensure you're on the right network
-      await (window as any).ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: '0x1' }] // Change to appropriate chainId (e.g. Arbitrum)
-      });
-  
       const accounts: string[] = await (window as any).ethereum.request({
         method: "eth_requestAccounts",
       });
       setUserWalletAddress(accounts[0]);
   
       const web3Provider = new ethers.providers.Web3Provider((window as any).ethereum);
+  
+      // Ask user to approve Ethereum address usage by signing a message
       const signer = web3Provider.getSigner();
-  
-      // Ask the user to sign a message for approval
-      const message = "Please sign this message to connect your wallet and approve its use on our site.";
-      const signature = await signer.signMessage(message);
-      console.log('Signature:', signature);
-  
-      // Reload the page to complete the connection process
-      window.location.reload();
+      const message = "Please approve to connect your wallet to our site.";
+      await signer.signMessage(message);
   
       // Once approved, check if the user is the contract owner or a medical provider
       checkContractOwner(web3Provider, accounts[0]);
